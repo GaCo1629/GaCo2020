d78b4206c233327d20a118c60320c2ad4a69d732
Phil Malone <robotsrulehumansdrool@gmail.com>
2020-01-23 17:59:13 -0500

    Converted to use hardware class
    Added BotBits class.java file

---
+++ src/main/java/frc/robot/BotBits.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 GaCo. All Rights Reserved.                              */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+// Public class to contain all the hardware elements (BotBits)
+public class BotBits {
+
+    public Joystick stick;
+    public static final int leftShooterCANid = 11;
+    public static final int rightShooterCANid = 12;
+    public CANSparkMax leftShooter; 
+    public CANSparkMax rightShooter;
+    public CANEncoder encoder;
+        
+    public void BotBits () {
+    }
+
+    public void init() {
+        stick = new Joystick(0);
+
+        // initialize motor
+        leftShooter = new CANSparkMax(leftShooterCANid, MotorType.kBrushless);
+        rightShooter = new CANSparkMax(rightShooterCANid, MotorType.kBrushless);
+        leftShooter.restoreFactoryDefaults();
+        rightShooter.restoreFactoryDefaults();
+    
+        // Encoder object created to display position/velocity values
+        encoder = leftShooter.getEncoder();    
+    }
+
+    public void show() {
+       SmartDashboard.putNumber("Speed", encoder.getVelocity());
+    }
+    
+}
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,33 +1,18 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* Copyright (c) 2020 GaCo                                                    */
 /*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANPIDController;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.ControlType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private Joystick m_stick;
-  private static final int deviceIDl = 11;
-  private static final int deviceIDr = 12;
-  private CANSparkMax m_lmotor; 
-  private CANSparkMax m_rmotor;
-  private CANEncoder m_encoder;
+
+  private BotBits botBits = new BotBits();;
 
   public double m_setpoint = 0.25;  
   public double m_speed = 0.0;
@@ -40,41 +25,22 @@ public class Robot extends TimedRobot {
   
   @Override
   public void robotInit() {
-    m_stick = new Joystick(0);
 
-    // initialize motor
-    m_lmotor = new CANSparkMax(deviceIDl, MotorType.kBrushless);
-    m_rmotor = new CANSparkMax(deviceIDr, MotorType.kBrushless);
-
-    /**
-     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
-     * parameters will not persist between power cycles
-     */
-    m_lmotor.restoreFactoryDefaults();
-    m_rmotor.restoreFactoryDefaults();
-
-    // Encoder object created to display position values
-    m_encoder = m_lmotor.getEncoder();
-
-    // display PID coefficients on SmartDashboard
-    SmartDashboard.putNumber("Setpoint", m_setpoint);
-    SmartDashboard.putNumber("Speed", m_speed);
+    botBits.init();
     
-
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("Setpoint", 0);
+    SmartDashboard.putNumber("Speed", 0);
   }
 
   @Override
   public void teleopPeriodic() {
-    // read PID coefficients from SmartDashboard
-    double p = SmartDashboard.getNumber("P Gain", 0);
-
     
-    if (m_stick.getRawButton(PAD_B)) {
+    if (botBits.stick.getRawButton(PAD_B)) {
       m_setpoint +=  0.002;
     }
     
-    if (m_stick.getRawButton(PAD_X)) {
+    if (botBits.stick.getRawButton(PAD_X)) {
       m_setpoint -=  0.002;
     }
 
@@ -84,24 +50,21 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Setpoint", m_setpoint * 100);
    
     
-    if (m_stick.getRawButton(PAD_Y)) {
+    if (botBits.stick.getRawButton(PAD_Y)) {
       m_enable = true;
-    } else if (m_stick.getRawButton(PAD_A)){
+    } else if (botBits.stick.getRawButton(PAD_A)){
       m_enable = false;
     }
 
     if (m_enable) {
-      m_lmotor.set(m_setpoint);
-      m_rmotor.set(-m_setpoint);
+      botBits.leftShooter.set(m_setpoint);
+      botBits.leftShooter.set(-m_setpoint);
     } else {
-      m_lmotor.set(0);
-      m_rmotor.set(0);     
+      botBits.leftShooter.set(0);
+      botBits.leftShooter.set(0);     
     }
 
-
-
-    m_speed = m_encoder.getVelocity();
-
-    SmartDashboard.putNumber("Speed", m_speed);
+    botBits.show();
+    
   }      
 }
