bb300021a4d14b0a4119638ab929da3faf5de9a1
WillMeagher <51809599+WillMeagher@users.noreply.github.com>
2020-01-30 18:30:04 -0500

    Update wheelOfFortune.java

--- src/main/java/frc/robot/wheelOfFortune.java
+++ src/main/java/frc/robot/wheelOfFortune.java
@@ -16,14 +16,48 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // Public class to contain all the hardware elements (BotBits)
 public class wheelOfFortune {
 
+    //constructor
     public  wheelOfFortune () {
     }
 
     public void init() {
+        //initialize motor to spin wheel
+        //initialize psunamatics to push out wheel spinner
+        //initialize camera/color sensor
+    }
+
+    //run the motor and use the sensor/camera so that the table turns 3.5 rotations
+    public void turnRotations(){
 
     }
 
-    public void move() {
+    //run the motor and use the sensor/camera so that the table turns to red
+    public void turnToRed(){
 
+    }
+
+    //run the motor and use the sensor/camera so that the table turns to blue
+    public void turnToBlue(){
+        
+    }
+
+    //run the motor and use the sensor/camera so that the table turns to green
+    public void turnToGreen(){
+        
+    }
+
+    //run the motor and use the sensor/camera so that the table turns to yellow
+    public void turnToYellow(){
+        
+    }
+
+
+    public void move() {
+        //Driver 2 - (Button) extend out mechanism
+        //Driver 2 - (Button) Spin wheel 3.5 rotations
+        //Driver 2 - (Button) Spin wheel to red
+        //Driver 2 - (Button) Spin wheel to blue
+        //Driver 2 - (Button) Spin wheel to green
+        //Driver 2 - (Button) Spin wheel to yellow
     }
 }
