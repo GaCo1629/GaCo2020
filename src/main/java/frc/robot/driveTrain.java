f24239ac2081eab7d7c1d093e330d57482c2eec9
WillMeagher <51809599+WillMeagher@users.noreply.github.com>
2020-01-29 20:50:28 -0500

    Create driveTrain.java
    created new classes for each mecansim sub system and starting to structure those classes

---
+++ src/main/java/frc/robot/driveTrain.java
@@ -0,0 +1,73 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 GaCo. All Rights Reserved.                              */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class driveTrain {
+
+    final double REDUCED_POWER_LEVEL  = .6;
+    final double BASELINE_POWER_LEVEL = .8;
+    final double EXTRA_POWER_LEVEL    =  1;
+
+    double axialPower;
+    double yawPower;
+
+    double robotHeading;
+    double targetHeading;
+
+    //constructor
+    public  driveTrain () {
+    }
+
+    //initalize hardware for the drive train
+    public void init(){
+        //initialize 3 right side motors
+        //initialize 3 right side motors
+        //initialize gyro
+
+        //initialize needed controller buttons
+
+    }
+
+    public void isTurning(){
+
+    }
+
+    public void getRobotHeading(){
+
+    }
+
+    public void driveRobot(){
+
+    }
+
+    public void autoCorrect(){
+
+    }
+
+    public void turnToHeading(double tragetHeading){
+
+    }
+
+    public void moveToPosition(double leftTarget, double rightTarget){
+        
+    }
+
+    public void move(){
+    //Driver 1 - left stick y drive forward/backward
+    //Driver 1 - right stick x turn left/right
+    //Driver 1 - (Button) Power mode
+    //Driver 1 - (Button) Slow mode
+    }
+
+}
