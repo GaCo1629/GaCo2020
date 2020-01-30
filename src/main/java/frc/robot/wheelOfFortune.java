7aec7fc12ef0f417ae3c32a1e355d64e6fd05068
WillMeagher <51809599+WillMeagher@users.noreply.github.com>
2020-01-29 20:51:13 -0500

    Create wheelOfFortune.java

---
+++ src/main/java/frc/robot/wheelOfFortune.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 GaCo. All Rights Reserved.                              */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+// Public class to contain all the hardware elements (BotBits)
+public class wheelOfFortune {
+
+    public  wheelOfFortune () {
+    }
+
+    public void init() {
+
+    }
+
+    public void move() {
+
+    }
+}
