4dbc95abb437d21d196ee2211aa0e490c3d37bf9
WillMeagher <51809599+WillMeagher@users.noreply.github.com>
2020-02-03 15:16:00 -0500

    Working on Gyroscope code and general additions

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -10,6 +10,8 @@ import com.revrobotics.CANSparkMaxLowLevel;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -40,7 +42,7 @@ public class DriveTrain {
     private CANEncoder rightDriveEncoder;
 
     //gyro
-    private AHRS ahrs; 
+    private AHRS gyro; 
 
     private final int leftDriveMasterCANid  = 1;
     private final int leftDriveFrontCANid   = 2;
@@ -53,11 +55,14 @@ public class DriveTrain {
     private final double BASELINE_POWER_LEVEL = .8;
     private final double EXTRA_POWER_LEVEL    =  1;
 
-    private double axialPower;
-    private double yawPower;
+    private double axial  = 0;
+    private double yaw    = 0;
 
-    private double robotHeading;
-    private double targetHeading;
+    private double absoluteHeading = 0;
+    private double robotHeading    = 0;
+    private double targetHeading   = 0;
+    private double robotHeadingModifier = 0;
+    private boolean turning = false;
 
     //constructor
     public  DriveTrain () {
@@ -77,21 +82,57 @@ public class DriveTrain {
         leftDriveEncoder  = leftDriveMaster.getEncoder();
         rightDriveEncoder = rightDriveMaster.getEncoder();
 
+        try {
+            gyro = new AHRS(SPI.Port.kMXP);
+          } catch (RuntimeException ex ) {
+            //insert report on driver station saying an error has occured
+        }
+
     }
 
     //check to see if the robot is turning
     public void isTurning(){
-
-    }
-
-    //return the currnet heading of the robot
-    public void getRobotHeading(){
-
+        if (Math.abs(axial) > 0.05) {
+            turning = true;
+            targetHeading = robotHeading;
+          }
+          else {
+            // Allow the robot to stop rotating and then lock in the heading.
+            if (turning) {
+              if (Math.abs(gyro.getRawGyroZ()) < 75) {
+                targetHeading = robotHeading;
+                turning = false;
+              }
+            }
+        }
     }
 
     //use the controller values to set axial and yaw values
-    public void driveRobot(){
+    public void setVectorsToController(){
+        axial = driverStation.getLeftStickY();
+        yaw   = driverStation.getRightStickX();
+    }
 
+    //return the currnet heading of the robot
+    public void getAbsoluteHeading(){
+        absoluteHeading = gyro.getAngle();
+    }
+
+    public void getRobotHeading(){
+        robotHeading = angleWrap180(robotHeading + robotHeadingModifier);
+    }
+
+    //resets gyro and sets headings to zero
+    public void resetHeading() {
+        gyro.zeroYaw();
+        robotHeading  = 0;
+        targetHeading = 0;
+        robotHeadingModifier = 0;
+    }
+
+    public void setHeading(double newHeading){
+        resetHeading();
+        robotHeadingModifier = newHeading;
     }
 
     //use the target heading and robot heading to modify the yaw value to continue driving strait
@@ -107,6 +148,16 @@ public class DriveTrain {
     //move the left and right motor position to target positions
     public void moveToPosition(double leftTarget, double rightTarget){
 
+    }
+
+    public double angleWrap180(double angle){
+            while(angle <= -180){
+                angle += 360;
+            }
+            while(angle >= 180){
+                angle -=360;
+            }
+            return angle;
     }
 
     public void move(){
