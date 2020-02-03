801b28c2a0656654692e5d9b67399aeb49290b1e
JDB03 <jalen.beeman@gmail.com>
2020-02-03 18:58:07 -0500

    Update ColorWheel.java

--- src/main/java/frc/robot/ColorWheel.java
+++ src/main/java/frc/robot/ColorWheel.java
@@ -9,14 +9,37 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
+import java.lang.Math;
+//import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 // Public class to contain all the hardware elements (BotBits)
 public class ColorWheel {
 
+
     DriverStation driverStation;
+
+    private CANSparkMax colorMotor;
+    private CANEncoder colorMotorEncoder;
+    //private Solenoid colorArm;
+    private DoubleSolenoid colorArm;
+    private final int colorMotorCANid  = 26;
+
+    private final double colorMotorDiameter = 6;
+    private final double colorWheelDiameter = 32;
+    private double colorMotorCircumference;
+    private double colorWheelCircumference;
+    private double maxRPM;
+    private double maxPower;
+    public double color;
+    public double curColor;
+    public int wheelCount;
+    
+
+
 
     // constructor
     public ColorWheel() {
@@ -24,6 +47,24 @@ public class ColorWheel {
 
     public void init(DriverStation driverStation) {
         this.driverStation = driverStation;
+
+        colorMotor  = new CANSparkMax(colorMotorCANid, CANSparkMaxLowLevel.MotorType.kBrushless);
+        colorMotor.restoreFactoryDefaults();
+
+        colorMotorEncoder = colorMotor.getEncoder();
+        /*colorArm = new Solenoid();
+        *This line depends on which line the piston is plugged into ^
+        */
+        colorArm = new DoubleSolenoid(1,2,3);
+        colorArm.set(DoubleSolenoid.Value.kReverse);
+        //This line has placeholder values ^
+
+        colorMotorCircumference = colorMotorDiameter * Math.PI;
+        colorWheelCircumference = colorWheelDiameter * Math.PI;
+        maxRPM = (60 * colorMotorCircumference)/colorWheelCircumference;
+        maxPower = (maxRPM)/5676;
+
+        //Find the maximum speed at which the wheel is allowed to spin.
 
         //initialize motor to spin wheel
         //initialize psunamatics to push out wheel spinner
