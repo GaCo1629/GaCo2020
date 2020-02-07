0be13d9264d6ce779f9165ca55a7c6e1c2332904
WillMeagher <51809599+WillMeagher@users.noreply.github.com>
2020-02-07 14:50:10 -0500

    Create PID class

---
+++ src/main/java/frc/robot/PIDController.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2020 GaCo. All Rights Reserved.                              */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+// Public class to contain all the hardware elements (BotBits)
+public class Climber {
+
+    DriverStation driverStation;
+
+    // constructor
+    public Climber(){
+    }
+
+
+    public void init(DriverStation driverStation) {
+        this.driverStation = driverStation;
+        //initialize both climbing motors
+        //initalize center of gravity adjustor motor
+    }
+
+    //extnnd up to get ready to climb
+    public void extendUp(){
+
+    }
+
+    //reset the climber to its origional position
+    public void reset(){
+
+    }
+
+    //pull down on climber with full power
+    public void climb(){
+
+    }
+
+    public void teleopPeriodic() {
+        //Driver 2 - (Button) Extend Up Climber
+        //Driver 2 - (Button) Climb up
+        //Driver 2 (Stick/buttons) reposition robot position
+    }
+}
